d6f37de0bb631e2dd152a4787062cd24023749f2
Adi Bev <adityabev@gmail.com>
2019-08-26 20:30:22 +0100

    Pagination added

--- src/components/ViewPokemons.js
+++ src/components/ViewPokemons.js
@@ -1,23 +1,24 @@
 import React, { Component } from "react";
-import axios from "axios";
-import { Card, Row, Col, Typography, Icon } from "antd";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { Card, Row, Col, Typography, Icon, Layout, Pagination } from "antd";
+
+import { fetchPokemons } from "../actions";
 
 const { Paragraph } = Typography;
 
 class GetPokemons extends Component {
   state = {
-    pokemons: ""
+    currentPage: 1
   };
 
-  componentDidMount = async () => {
-    let res = await axios.get("http://localhost:5000/api/pokemons");
-    let pokemonData = await res.data;
-    this.setState({ pokemons: pokemonData });
-    console.log(this.state.pokemons);
+  componentDidMount = () => {
+    this.props.fetchPokemons(this.state.currentPage);
   };
 
-  mapStatePokemon = pokemons => {
-    return pokemons.map(pokemon => (
+  mapPokemonData = pokemons => {
+    return this.props.pokemonData.pokemons.map(pokemon => (
       <Col
         xs={{ span: 24 }}
         sm={{ span: 12 }}
@@ -33,8 +34,13 @@ class GetPokemons extends Component {
     ));
   };
 
+  handlePaginationChange = async page => {
+    await this.setState({ currentPage: page });
+    await this.props.fetchPokemons(this.state.currentPage);
+  };
+
   render() {
-    if (!this.state.pokemons) {
+    if (!this.props.pokemonData) {
       return (
         <div className="loading-container">
           <Icon type="loading" />
@@ -42,13 +48,54 @@ class GetPokemons extends Component {
       );
     }
     return (
-      <div className="viewContainer">
-        <Row justify="start" gutter={16}>
-          {this.mapStatePokemon(this.state.pokemons)}
-        </Row>
-      </div>
+      <Layout>
+        <div className="viewContainer">
+          <Row className="pagination-top">
+            <Col xs={{ span: 24 }}>
+              <Pagination
+                current={this.state.currentPage}
+                onChange={this.handlePaginationChange}
+                pageSize={9}
+                total={this.props.pokemonData.totalRecords}
+              />
+            </Col>
+          </Row>
+
+          <Row justify="start" gutter={16}>
+            {this.mapPokemonData(this.props.pokemonData)}
+          </Row>
+
+          <Row className="pagination-bottom">
+            <Col xs={{ span: 24 }}>
+              <Pagination
+                current={this.state.currentPage}
+                onChange={this.handlePaginationChange}
+                pageSize={9}
+                total={this.props.pokemonData.totalRecords}
+              />
+            </Col>
+          </Row>
+        </div>
+      </Layout>
     );
   }
 }
 
-export default GetPokemons;
+const mapStateToProps = ({ pokemonData }) => {
+  return { pokemonData };
+};
+
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators({ fetchPokemons }, dispatch);
+};
+
+GetPokemons.propTypes = {
+  pokemonData: PropTypes.oneOfType([PropTypes.string, PropTypes.object])
+    .isRequired,
+  fetchPokemons: PropTypes.func.isRequired
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(GetPokemons);
