b6f3530b2abd5211f461240e15ac037981c63315
Adi Bev <adityabev@gmail.com>
2019-08-26 20:31:04 +0100

    redux store connected to the app

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
 import "antd/dist/antd.css";
 import "./styles/index.css";
+
+//importing redux store here
+import store from "./store";
 
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <Router>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </Router>,
   document.getElementById("root")
 );
