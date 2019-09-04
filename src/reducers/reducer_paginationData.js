9bed92e8386ab28b0a7bb4cf21ed754a1405c333
Adi Bev <adityabev@gmail.com>
2019-09-04 22:34:21 +0100

    pagination data reducer added

---
+++ src/reducers/reducer_paginationData.js
@@ -0,0 +1,11 @@
+import { GET_DATA } from "../actions/index";
+
+export default (state = "", action) => {
+  switch (action.type) {
+    case GET_DATA:
+      return action.payload;
+
+    default:
+      return state;
+  }
+};
