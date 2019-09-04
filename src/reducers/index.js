eac735b2775b61d2c0724f28a0d88aa4ad9031ad
Adi Bev <adityabev@gmail.com>
2019-09-04 22:33:45 +0100

    existing state modified, new states added

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,15 @@
 import { combineReducers } from "redux";
 
 import postPokemonReducer from "./reducer_postPokemon";
-import getPokemonsReducer from "./reducer_getPokemons";
+import paginationDataReducer from "./reducer_paginationData";
+import updatePokemonReducer from "./reducer_updatePokemon";
+import pokemonDataReducer from "./reducer_pokemonData";
 
 const rootReducer = combineReducers({
   postStatus: postPokemonReducer,
-  pokemonData: getPokemonsReducer
+  updateStatus: updatePokemonReducer,
+  paginationData: paginationDataReducer,
+  pokemonList: pokemonDataReducer
 });
 
 export default rootReducer;
