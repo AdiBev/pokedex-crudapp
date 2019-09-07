5db9eee9ea7b96de0d076c4e7c3edc5cea0a2229
Adi Bev <adityabev@gmail.com>
2019-09-07 19:17:58 +0100

    New state introduced

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,12 +4,14 @@ import postPokemonReducer from "./reducer_postPokemon";
 import paginationDataReducer from "./reducer_paginationData";
 import updatePokemonReducer from "./reducer_updatePokemon";
 import pokemonDataReducer from "./reducer_pokemonData";
+import nearbyPokemonsReducer from "./reducer_nearbyPokemons";
 
 const rootReducer = combineReducers({
   postStatus: postPokemonReducer,
   updateStatus: updatePokemonReducer,
   paginationData: paginationDataReducer,
-  pokemonList: pokemonDataReducer
+  pokemonList: pokemonDataReducer,
+  nearbyPokemons: nearbyPokemonsReducer
 });
 
 export default rootReducer;
