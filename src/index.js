b6b0f9bdccadd4579e56ffbc8e280bb2f3eafd9a
Adi Bev <adityabev@gmail.com>
2019-09-07 19:15:50 +0100

    index.scss file imported

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import "antd/dist/antd.css";
-import "./styles/index.css";
+import "./styles/index.scss";
 
 //importing redux store here
 import store from "./store";
